d7ec5364d3a13579b89de9ce2b2903d7901bf501
李粉娜 <1303881850@qq.com>
2020-03-02 13:23:59 +0800

    前++ &  后++

---
+++ test19_11_18/test_1118/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+	int a = 0;
+	printf("%d %d %d %d \n", a++, ++a, a++, ++a);
+	//int b = (a++)+(a++);
+	//int b = (++a) + (a++);
+	//int b = (a++) + (++a);
+	//int b = (a++) + (a++) + (a++);
+	//int b = (++a) + (a++) + (a++);
+	//int b = (a++) + (a++) + (++a);
+	//printf("b=%d\n",b);
+	return;
+}
+
+
+
+
+
