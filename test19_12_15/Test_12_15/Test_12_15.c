b06d8aca5ddf591ae9e3e53f442b830d6e77309d
lifenna2019 <1303881850@qq.com>
2020-03-11 21:28:44 +0800

    1.自我实现my_size 2.自我实现my_strcpy

--- test19_12_15/Test_12_15/Test_12_15.c
+++ test19_12_15/Test_12_15/Test_12_15.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 #include<assert.h>
-
+//指针函数，两个字符指针作为形参，函数返回一个字符指针
 char *my_strcpy(char *strDest, char *strSrc)
-{	
+{
 	int count1 = 0;
 	int count2 = 0;
 	assert((strSrc != NULL)&&(strDest!=NULL));
@@ -30,13 +30,13 @@ char *my_strcpy(char *strDest, char *strSrc)
 //}
 int main()
 {
-	char dest[] = "hellobit";
-	char *src = "Bit.";
+	char dest[] = "Hello Word";
+	char *src = "sun.";
 		//int len;
 		//len = my_size(str);
-	printf("dest=%s", dest);
+	//printf("dest=%s", dest);
 	my_strcpy(dest, src);
-	//printf("len=%d", len);
+	   //printf("len=%d", len);
 	printf("dest=%s", dest);
 	return 0;
 }
