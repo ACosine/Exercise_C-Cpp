c1b9262d91aeef7fb98f4736fcf6f8bb137ade9d
lifenna2019 <1303881850@qq.com>
2020-03-27 23:02:48 +0800

    身份证归属地查询

---
+++ test19_11_23/test_11_23/common.h
@@ -0,0 +1,12 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<windows.h>
+
+#define MAX_BUFFER_SIZE 256
+#define MAX_CMD_SIZE 128
+
+#endif /* _COMMON_H_ */
---
+++ test19_11_23/test_11_23/sysframe.h
@@ -0,0 +1,17 @@
+#ifndef _SYS_FRAME_H_
+#define _SYS_FRAME_H_
+
+#include "common.h"
+//设置光标位置
+void SetCurPos(int x, int y);
+void HideCursor();
+void RecoverCursor();
+
+//框架设置
+void DrawRow(int x, int y);  
+void DrawCol(int x, int y);
+void DrawFrame(const char *title);
+void SystemEnd();
+
+
+#endif /* _SYS_FRAME_H_ */
