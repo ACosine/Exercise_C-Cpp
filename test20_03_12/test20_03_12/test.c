e630347d828688db92c41cbc99d3046e694548ad
lifenna2019 <1303881850@qq.com>
2020-03-30 22:48:54 +0800

    递归：待完成

--- test20_03_12/test20_03_12/test.c
+++ test20_03_12/test20_03_12/test.c
@@ -3,7 +3,7 @@
 #include<assert.h>
 /************************************* 打印一个整数的每一位**************************************************/
 
-//void Print_bit(int num)
+// void Print_bit(int num)
 //{
 //	if(num>9)//两位数以上进行递归 
 //	{
@@ -14,46 +14,56 @@
 //}
 //int main()
 //{
-//	int num = 0;
 //	printf("请输入要打印的数：\n");
 //	scanf("%d", &num);
+//	int num = 0;
 //	Print_bit(num);
 //	return 0;
 //}
 
 /************************************* n的阶乘（不考虑溢出的问题）*******************************************/
 
-int Factorial1(int n)//非递归法
-{
-
-}
-
-int Factorial2(int n)//递归法
-{
-	assert(n > 0);//负数没有阶乘
-	if (n <=1)//0和1的阶乘为1
-	{
-		return 1;
-	}
-	else
-	{
-		return n*Factorial(n - 1);//大于1的进行递归
-	}
-}
-int main()
-{
-	
-	int n = 0,Res = 0;
-	printf("请输入要计算的数阶乘：\n");
-	scanf("%d", &n);   // n超过12就会溢出
-	Res=Factorial(n);
-	printf("Res=%d\n", Res);
-	return 0;
-}
+//int Factorial1(int n)//非递归法
+//{
+//	assert(n > 0);//负数没有阶乘
+//	int res = 1;
+//	for (int i = n; i > 1; --i)
+//	{
+//		res *= i;
+//	}
+//	return res;
+//}
+//
+//int Factorial2(int n)//递归法
+//{
+//	assert(n > 0);//负数没有阶乘
+//	if (n <=1)//0和1的阶乘为1
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return n*Factorial(n - 1);//大于1的进行递归
+//	}
+//}
+//int main()
+//{
+//	
+//	int n = 0,Res = 0;
+//	printf("请输入要计算的数阶乘：\n");
+//	scanf("%d", &n);   // n超过12就会溢出
+//	Res=Factorial1(n);
+//	printf("Res=%d\n", Res);
+//	return 0;
+//}
 
 
 /************************************* my_strlen *************************************************************/
 
+int mian()
+{
+	return 0;
+}
 
 /************************************* 字符串逆序 ************************************************************/
 
