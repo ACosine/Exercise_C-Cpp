51cbd9e1e4f1d525357590f0114b0d5fe0b34062
lifenna2019 <1303881850@qq.com>
2020-10-13 15:46:32 +0800

    excise

---
+++ test20_05_25/test_05_25/test.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1  
+#include<stdio.h>
+//#include<memory.h>
+//int main()
+//{
+//	union
+//	{
+//		short k;
+//		char i[2];
+//	}*s, a;
+//	s = &a;
+//	s->i[0] = 0x39;
+//	s->i[1] = 0x38;
+//	printf("%x\n", a.k);
+//	return 0;
+//}
+//int main()
+//{
+//	unsigned char puc[4];
+//	struct tagPIM
+//	{
+//		unsigned char ucPim1;
+//		unsigned char ucData0 : 1;//位段
+//		unsigned char ucData1 : 2;
+//		unsigned char ucData2 : 3;
+//	}*pstPimData;
+//	pstPimData = (struct tagPIM*)puc;
+//	memset(puc, 0, 4);
+//	pstPimData->ucPim1 = 2;
+//	pstPimData->ucData0 = 3;
+//	pstPimData->ucData1 = 4;
+//	pstPimData->ucData2 = 5;
+//	printf("%02x %02x %02x %02x\n", puc[0], puc[1], puc[2], puc[3]);
+//	return 0;
+//}
+
+
+int main()
+{
+	char a = 0, ch;
+	while ((ch = getchar()) != '\n')
+	{
+		if (a % 2 != 0 && (ch >= 'a'&&ch <= 'z'))//位于奇数位置且是小写字母的转换为大写字母
+			ch = ch - 'a' + 'A';
+		a++;
+		putchar(ch);
+
+
+	}
+	printf("\n");
+}
